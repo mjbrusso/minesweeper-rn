9d4c70e621332bb918c4a92fa5bedd6bc3e593c1
mjbrusso <mjbrusso@gmail.com>
2019-05-28 13:26:43 -0300

    Add Params.js

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+import { Dimensions } from "react-native";
+
+export const Params = {
+  blockSize: 30,
+  borderSize: 5,
+  fontSize: 30,
+  headerRatio: 0.15,
+  difficultLevel: 0.1,
+
+  getColumnsCount() {
+    const width = Dimensions.get("window").width;
+    return Math.floor(width / this.blockSize);
+  },
+  getRowsCount() {
+    const totalHeight = Dimensions.get("window").width;
+    const boardHeight = totalHeight * (1 - this.headerRatio);
+
+    return Math.floor(boardHeight / this.blockSize);
+  }
+};
+
+export default Params;
